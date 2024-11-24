2852954fe81dee734fb915142e3fa1fbd47dc33d
LeAnne B. <branchleannelmb@gmail.com>
2024-11-24 16:44:02 -0500

    gets snake moving

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -106,7 +106,12 @@ public class GamePanel extends JPanel implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
 		
 	}
 
